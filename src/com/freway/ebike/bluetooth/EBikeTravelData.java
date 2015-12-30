a289900865d341714381d8bad56023c85411ba03
Nicolls <851778509@qq.com>
2015-12-30 14:32:45 +0800

    去掉存储单位转换

--- src/com/freway/ebike/bluetooth/EBikeTravelData.java
+++ src/com/freway/ebike/bluetooth/EBikeTravelData.java
@@ -312,18 +312,17 @@ public class EBikeTravelData implements Serializable {
 			BaseApplication.travelId = -1;
 		} else {
 			formatFloat2OneAccuracy();
-			//因为服务器显示的是迈,所以在存储数据时要做一些处理.
 			Travel travel = new Travel();
 			travel.setId(travelId);
 			travel.setType(type);
 			travel.setSync(0);
 			travel.setAltitude(altitude);
-			travel.setAvgSpeed(avgSpeed / 1.6f);
+			travel.setAvgSpeed(avgSpeed);
 			travel.setCadence(cal_recordCadence);
 			travel.setCalorie(calorie);
-			travel.setDistance(distance * 0.6f);
+			travel.setDistance(distance);
 			travel.setEndTime(endTime);
-			travel.setMaxSpeed(maxSpeed/1.6f);
+			travel.setMaxSpeed(maxSpeed);
 			travel.setSpendTime(spendTime);
 			travel.setStartTime(startTime);
 			travel.setPhoto(travelPhoto);
@@ -573,14 +572,13 @@ public class EBikeTravelData implements Serializable {
 			} else {
 				formatFloat2OneAccuracy();
 				if (!isReInitCaculate) {// 说明有数据 //dataId为0说明读完了，保存起来
-					//服务器存储的是迈,所以要处理一下数据
 					Travel travel = new Travel();
 					travel.setId(travelId);
-					travel.setAvgSpeed(avgSpeed/1.6f);
+					travel.setAvgSpeed(avgSpeed);
 					travel.setCadence(cal_recordCadence);
 					travel.setCalorie(calorie);
-					travel.setDistance(distance * 0.6f);
-					travel.setMaxSpeed(maxSpeed/1.6f);
+					travel.setDistance(distance);
+					travel.setMaxSpeed(maxSpeed);
 					travel.setSpendTime(spendTime);
 					DBHelper.getInstance(context).updateTravel(travel);
 					if (netUtil == null) {
@@ -738,8 +736,7 @@ public class EBikeTravelData implements Serializable {
 					if (spendTime != 0 && (spendTime % RECORD_TIME_FRE) == 0) {// 每10秒存储一个速度
 						TravelSpeed travelSpeed = new TravelSpeed();
 						travelSpeed.setTravelId(travelId);
-						//服务器存储的是迈,所以要处理一下
-						travelSpeed.setSpeed(CommonUtil.formatFloatAccuracy(avgSpeed/ 1.6f, 1));
+						travelSpeed.setSpeed(CommonUtil.formatFloatAccuracy(avgSpeed, 1));
 						DBHelper.getInstance(context).insertTravelSpeed(travelSpeed);
 					}
 				}
